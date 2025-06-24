933e475353e3baad82ebab563d2a839c78cfff27
Halhulk <oktemh2@gmail.com>
2025-06-24 20:11:53 +0300

    another update

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/cosmic-greetings/',
-  plugins: [react()],
-  optimizeDeps: {
-    include: ['i18next-browser-languagedetector']
-  }
+  plugins: [react()]
 });
