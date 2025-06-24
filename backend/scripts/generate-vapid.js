ffa5c99590cdaf4c179a672982d5cc043a3dc8e2
Halhulk <oktemh2@gmail.com>
2025-06-24 17:41:03 +0300

    Simplified folders

--- backend/scripts/generate-vapid.js
+++
@@ -1,8 +0,0 @@
-// Generate a new pair of VAPID keys for Web Push
-// Usage: node generate-vapid.js
-
-const webpush = require('web-push');
-
-const keys = webpush.generateVAPIDKeys();
-console.log('VAPID_PUBLIC=' + keys.publicKey);
-console.log('VAPID_PRIVATE=' + keys.privateKey);
